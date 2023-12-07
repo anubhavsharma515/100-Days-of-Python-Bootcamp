eca5adccf3e59940114c6282dd0a491685500267
Anubhav Sharma <anubhav.sharma@thescore.com>
2023-12-06 22:19:53 -0500

    Improve hangman.py some more

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -1,41 +1,37 @@
 
-
 word = 'beekeeper'
 word_length = len(word)
 guesses = ['_' for _ in range(0, word_length)]
 
-wrong_guesses = 0
-word_guessed = False
-
-letters_used = []
-
-def check_letter_used(letter: str):
-  if letter in letters_used:
-    return True
-  
-  letters_used.append(letter)
-  return False
+lives = 6
+end_of_game = False
 
 def update_guess(letter: str):
   for i in range(0, word_length): 
     if word[i] == letter: 
       guesses[i] = letter
 
-while wrong_guesses < 6 and not(word_guessed):
+while not end_of_game:
+
   letter = input("Guess a Letter: ")
+
   if letter in word:
     letter_guessed = check_letter_used(letter)
-    if letter_guessed:
+
+    if letter_guessed in guesses:
       print(f"You already guessed {letter}")
     else:
       update_guess(letter)
       print(guesses)
 
     if (len(guesses) == word_length):
-      word_guessed = True
+      end_of_game = True
 
   else:
-    print('XXXXXXXXXXXXX')
-    wrong_guesses += 1
-    print(f"Guesses left: {6 - wrong_guesses}")
-    
+    if lives > 0:
+      lives -= 1
+      print('XXXXXXXXXXXXX')
+      print(f"Guesses left: {6 - lives}")
+    else:
+      end_of_game = True
+      
